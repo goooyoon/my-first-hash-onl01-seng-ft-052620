8e7ea7a64d30a9213ec2901d2c88e24359160ede
Grace Kim <goooyoon@gmail.com>
2020-05-30 04:59:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
 the_manifest = hash
-hash = {"key1" => "value1"}
+hash = {"whale bone corsets" => 5
+"porcelain vases" => 2
+"oil paintings" => 3}
 end
 
 def retrieval
